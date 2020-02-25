704340b2347d844ac8e625a44e59cb85848d5b10
Jitender85 <jitenderwebdesigner@gmail.com>
2020-02-25 10:33:07 +0530

    Updated APP version.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://jitender:jitu1234@cluster0-f9hls.mongodb.net/test?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://jitender:jitu1234@pgstays-den8a.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey: "bEjR9GOERvA5qrgTXXiPksTg9l"
 };
